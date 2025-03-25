d2fd148400299877aad226bd39aacff2275629e6
Prajit Kumar Das <prajitdas@gmail.com>
2025-03-24 21:52:15 -0400

    tracker update

--- tracker.py
+++ tracker.py
@@ -8,8 +8,11 @@ import logging
 CONST_CONFIG_PATH = "config.ini"
 CONST_BITCOIN_PRICE_FILE_PATH = "btc.json"
 CONST_BTC_LOG_FILE_PATH = "btc.log"
-CONST_SECTION_NAME = "API"
-CONST_KEY_NAME="api_key"
+CONST_API_SECTION_NAME = "API"
+CONST_API_KEY_NAME="api_key"
+CONST_TELEGRAM_SECTION_NAME="TELEGRAM"
+CONST_TELEGRAM_BOT_TOKEN_NAME="bot_token"
+CONST_TELEGRAM_CHAT_ID_NAME="chat_id"
 CONST_PERCENT_CHANGE = 1
 CONST_TIME_INTERVAL = 5 * 60 #Can't make more calls than one every 5 min; CoinMarketCap API restrictions
 
@@ -87,16 +90,28 @@ def get_btc_price(api_key):
     btc_price = response_json["data"][0]
     return btc_price["quote"]["USD"]["price"]
     
+# fn to send_message through telegram
+def send_message(msg, console_logger, bot_token, chat_id):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
+    try:
+        # send the msg
+        r = requests.get(url)
+        console_logger.info(r.status_code)
+        console_logger.info(r.json())
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
 def btc_calc_delta(price1, price2):
     return ((price2 - price1) / price1) * 100
 
-def track_btc_price(api_key, console_logger):
+def track_btc_price(api_key, console_logger, bot_token, chat_id):
     btc_curr_price = btc_prev_price = get_btc_price(api_key)
 
     # infinite loop
     while True:
         console_logger.info("Tracking BTC price...")
+        send_message("Tracking BTC price...", console_logger, bot_token, chat_id)
+        console_logger.info("Started at: " + str(time.time()))
         console_logger.info(f"BTC price is: {btc_curr_price}")
         write_btc_price_list_to_json({"price": btc_curr_price, "time": time.time()})
         btc_curr_price = get_btc_price(api_key)
@@ -107,10 +122,13 @@ def track_btc_price(api_key, console_logger):
             console_logger.info(f"BTC price changed by: {CONST_PERCENT_CHANGE}")
             console_logger.info(f"Current price: {btc_curr_price}")
             console_logger.info(f"Previous price: {btc_prev_price}")
+            msg=""
             if delta > 0:
-                console_logger.info(f"Price increased by: {delta}")
+                msg=f"Price increased by: {delta}"
             else:
-                console_logger.info(f"Price decreased by: {delta}")
+                msg=f"Price decreased by: {delta}"
+            send_message(msg, console_logger, bot_token, chat_id)
+            console_logger.info(msg)
             btc_prev_price = btc_curr_price
         
         # fetch the price for every dash minutes
@@ -120,12 +138,17 @@ def main():
     if not os.path.exists(CONST_CONFIG_PATH):
         console_logger.info(f"Config file not found at '{CONST_CONFIG_PATH}', cannot proceed.")
         return
-    api_key = read_config(CONST_CONFIG_PATH, CONST_SECTION_NAME, CONST_KEY_NAME)
+    api_key = read_config(CONST_CONFIG_PATH, CONST_API_SECTION_NAME, CONST_API_KEY_NAME)
     if not api_key:
         console_logger.info(f"API key not found in the config file '{CONST_CONFIG_PATH}', cannot proceed.")
         return
+    bot_token=read_config(CONST_CONFIG_PATH, CONST_TELEGRAM_SECTION_NAME, CONST_TELEGRAM_BOT_TOKEN_NAME)
+    chat_id=read_config(CONST_CONFIG_PATH, CONST_TELEGRAM_SECTION_NAME, CONST_TELEGRAM_CHAT_ID_NAME)
+    if not bot_token or not chat_id:
+        console_logger.info("Bot token or chat id not found in the config file, cannot proceed.")
+        return
     console_logger = configure_logger_with_console()
-    track_btc_price(api_key, console_logger)
+    track_btc_price(api_key, console_logger, bot_token, chat_id)
 
 # fancy way to activate the main() function
 if __name__ == "__main__":
